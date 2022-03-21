743b0989f3b0bdb8052a153b03d8587df090944a
dyhmzall <dyhmzall@gmail.com>
2022-03-22 01:20:50 +0300

    Домашнее задание после 4 урока

---
+++ todo/paginations.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
